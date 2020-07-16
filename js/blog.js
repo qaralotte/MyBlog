59f21d4d49e51e307b35a3907828236bb3ab58b1
qaralotte <qaralotte@gmail.com>
2020-07-17 03:23:03 +0800

    update at 2020-7-17 03:23

---
+++ js/blog.js
@@ -0,0 +1,30 @@
+/*
+ *   顶部判断
+ */
+window.onscroll = function () {
+    if (document.documentElement.scrollTop !== 0) {
+        document.getElementById("header_top").id = 'header_scroll';
+        document.getElementById("title_top").id = 'title_scroll';
+        document.getElementById("go_top").style.opacity = "1";
+        document.getElementById("go_top").style.pointerEvents = "initial";
+    } else {
+        document.getElementById("header_scroll").id = 'header_top';
+        document.getElementById("title_scroll").id = 'title_top';
+        document.getElementById("go_top").style.opacity = "0";
+        document.getElementById("go_top").style.pointerEvents = "none";
+    }
+}
+
+/*
+ *    返回顶部按钮
+ */
+
+function goTop() {
+    (function smoothscroll() {
+        const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+        if (currentScroll > 0) {
+            window.requestAnimationFrame(smoothscroll);
+            window.scrollTo(0, currentScroll - (currentScroll / 5));
+        }
+    })();
+}
